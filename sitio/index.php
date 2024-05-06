566bcd2789fe680d6b9b89d679b0e8eafd33e340
ivancito10 <ivanphico10@gmail.com>
2024-05-05 21:52:39 -0400

    Se agrega el Ejercicio

---
+++ sitio/index.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <form action="http://localhost:8080/WebApplication1/NewServlet" method="GET">
+            nombre <input type="text" name="nombre"/><br>
+            apellido <input type="text" name="apellido"/><br>
+            <input type="submit" name="Enviar" value="Enviar"/>
+        </form>
+        <hr>
+        <form action="http://localhost:51106/Default.aspx" method="GET">
+            nombre <input type="text" name="nombre"/><br>
+            apellido <input type="text" name="apellido"/><br>
+            <input type="submit" name="Enviar" value="Enviar"/>
+        </form>
+    </body>
+</html>
